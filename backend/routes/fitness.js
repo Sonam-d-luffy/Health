5a5ad073cad0e777cf529927febf77a0f1722d1e
Sonam Kumari <sonam992007@gmail.com>
2026-09-03 01:51:14 +0530

    'changes'

--- backend/routes/fitness.js
+++ backend/routes/fitness.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-const fitnessSchema = new mongoose.Schema({
+export const fitnessSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",

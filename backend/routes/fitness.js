6f01b8458a6366bcd6f5f491e9454e64ef0e27e9
Sonam Kumari <sonam992007@gmail.com>
2026-09-03 02:19:37 +0530

    'changes'

--- backend/routes/fitness.js
+++ backend/routes/fitness.js
@@ -1,59 +1,75 @@
-import mongoose from 'mongoose'
-export const fitnessSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-    unique: true
-  },
+import Fitness from "../models/fitness.js";
 
-  bicepCurls: {
-    maxCount: {
-      type: Number,
-      default: 0
-    },
-    totalTime: {
-      type: Number, // seconds
-      default: 0
-    }
-  },
+export const saveExercise = async (req, res) => {
+  try {
+    const {
+      userId,
+      exercise,
+      count,
+      time
+    } = req.body;
 
-  squats: {
-    maxCount: {
-      type: Number,
-      default: 0
-    },
-    totalTime: {
-      type: Number, // seconds
-      default: 0
+    if (!userId || !exercise) {
+      return res.status(400).json({
+        success: false,
+        message: "userId and exercise are required"
+      });
     }
-  },
 
-  pullUps: {
-    maxCount: {
-      type: Number,
-      default: 0
-    },
-    totalTime: {
-      type: Number, // seconds
-      default: 0
-    }
-  },
+    const exerciseMap = {
+      bicep_curl: "bicepCurls",
+      squat: "squats",
+      pullup: "pullUps",
+      jogging: "jogging"
+    };
 
-  jogging: {
-    maxCount: {
-      type: Number,
-      default: 0
-    },
-    totalTime: {
-      type: Number, // seconds
-      default: 0
+    const field = exerciseMap[exercise];
+
+    if (!field) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid exercise"
+      });
     }
+
+    let fitness = await Fitness.findOne({
+      user: userId
+    });
+
+    if (!fitness) {
+      fitness = new Fitness({
+        user: userId
+      });
+    }
+
+    // Make sure nested object exists
+    if (!fitness[field]) {
+      fitness[field] = {
+        maxCount: 0,
+        maxCountTime: 0
+      };
+    }
+
+    // Update only if this workout is a new maximum
+    if (Number(count) > fitness[field].maxCount) {
+      fitness[field].maxCount = Number(count);
+      fitness[field].maxCountTime = Number(time) || 0;
+    }
+
+    await fitness.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Fitness data updated",
+      data: fitness
+    });
+
+  } catch (error) {
+    console.error("Fitness update error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Server error"
+    });
   }
-}, {
-  timestamps: true
-});
-
-const Fitness = mongoose.model("Fitness", fitnessSchema);
-
-export default Fitness;
+};

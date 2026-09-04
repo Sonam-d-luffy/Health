d36927f1767bc186e17eab3c85d9f3a1279af06e
Sonam Kumari <sonam992007@gmail.com>
2026-09-05 02:46:42 +0530

    'changse'

--- backend/server.js
+++ backend/server.js
@@ -55,9 +55,11 @@ mongoose.connect(process.env.MONGO_URI)
  
 
 
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
+    const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
   })
   .catch(error => {
     console.error("MongoDB connection error:", error);
